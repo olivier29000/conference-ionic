19b6293cf28c8bb417ea9c8595722838dd586bc2
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-10-09 15:25:21 +0200

    deploiement Travis

--- src/app/services/programme.service.ts
+++ src/app/services/programme.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Journee } from '../models/journee';
 
 
 const httpOptions = {
